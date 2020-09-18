b33e7a3dbb055caee5ada0ee3d6c2a774488bab3
Ray Vernagus <rvernagus@hotmail.com>
2020-09-18 15:05:22 +0000

    Add reverse index example

--- fsharp/5/1-slicing.fsx
+++ fsharp/5/1-slicing.fsx
@@ -1,11 +1,11 @@
-let l = [ 1..10 ]
+let l = [1..10]
 printfn $"%A{l}"
 
-let a = [| 1..10 |]
+let a = [|1..10|]
 printfn $"%A{a}"
 
-printfn $"%A{l.[ 3..6 ]}"
-printfn $"{l.[ 11..15 ]}"
+printfn $"%A{l.[3..6]}"
+printfn $"{l.[11..15]}"
 
 // Slicing 3D arrays
 let dim = 2
@@ -20,3 +20,7 @@ for z in 0..dim-1 do
             cnt <- cnt + 1
             
 printfn $"%A{m.[*, 0, 1]}"
+
+let xs = [1..10]
+printfn $"{xs.[^1]}"
+printfn $"{xs.[^2..]}"
