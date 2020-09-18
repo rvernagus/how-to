45a4f42a03b6b32c6159d0392cc188a3e0ba224b
Ray Vernagus <rvernagus@hotmail.com>
2020-09-18 15:05:36 +0000

    Add custom slice example

---
+++ fsharp/5/2-custom-slice.fsx
@@ -0,0 +1,29 @@
+open System
+
+type Span<'T> with
+    member sp.GetSlice(startIdx, endIdx) =
+        let s = defaultArg startIdx 0
+        let e = defaultArg endIdx sp.Length
+        sp.Slice(s, e - s)
+    
+    member sp.GetReverseIndex(_, offset: int) =
+        sp.Length - offset
+
+let printSpan (sp: Span<int>) =
+    let arr = sp.ToArray()
+    printfn "%A" arr
+
+let run () =
+    let sp = [| 1; 2; 3; 4; 5 |].AsSpan()
+    
+    // Pre-# 5.0 slicing on a Span<'T>
+    printSpan sp.[0..] // [|1; 2; 3; 4; 5|]
+    printSpan sp.[..3] // [|1; 2; 3|]
+    printSpan sp.[1..3] // |2; 3|]
+    
+    // Same slices, but only using from-the-end index
+    printSpan sp.[..^0] // [|1; 2; 3; 4; 5|]
+    printSpan sp.[..^2] // [|1; 2; 3|]
+    printSpan sp.[^4..^2] // [|2; 3|]
+
+run()
