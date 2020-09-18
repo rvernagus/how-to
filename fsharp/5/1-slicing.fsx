0b4f8969eab038f94be12fa182f9adf0feed9f9f
Ray Vernagus <rvernagus@hotmail.com>
2020-09-18 02:50:20 +0000

    Add F# 5 slicing examples

---
+++ fsharp/5/1-slicing.fsx
@@ -0,0 +1,22 @@
+let l = [ 1..10 ]
+printfn $"%A{l}"
+
+let a = [| 1..10 |]
+printfn $"%A{a}"
+
+printfn $"%A{l.[ 3..6 ]}"
+printfn $"{l.[ 11..15 ]}"
+
+// Slicing 3D arrays
+let dim = 2
+let m = Array3D.zeroCreate<int> dim dim dim
+
+let mutable cnt = 0
+
+for z in 0..dim-1 do
+    for y in 0..dim-1 do
+        for x in 0..dim-1 do
+            m.[x,y,z] <- cnt
+            cnt <- cnt + 1
+            
+printfn $"%A{m.[*, 0, 1]}"
