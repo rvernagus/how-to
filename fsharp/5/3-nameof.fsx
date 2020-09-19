ea973731f017f52504ed9ac3531525dcf32c94b6
Ray Vernagus <rvernagus@hotmail.com>
2020-09-19 18:12:17 +0000

    Additional nameof example

--- fsharp/5/3-nameof.fsx
+++ fsharp/5/3-nameof.fsx
@@ -4,3 +4,15 @@ module M =
 printfn $"{M.f 12}"
 printfn $"{nameof M}"
 printfn $"{nameof M.f}"
+
+type Person =
+    { Name : string
+      Age : int }
+
+let reflectOnPerson p prop =
+    match prop with
+    | nameof p.Name -> p.Name
+    | nameof p.Age  -> string p.Age
+
+printfn $"""{reflectOnPerson {Name="Ray"; Age=47} "Name"}"""
+printfn $"""{reflectOnPerson {Name="Ray"; Age=47} "Age" }"""
