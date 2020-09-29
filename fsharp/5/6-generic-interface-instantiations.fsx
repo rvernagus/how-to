d82760ced9cec61b0575c84eb0e1ee39f67751bf
Ray Vernagus <rvernagus@hotmail.com>
2020-09-29 21:46:42 +0000

    Add generic interface instantiation example

---
+++ fsharp/5/6-generic-interface-instantiations.fsx
@@ -0,0 +1,15 @@
+type IA<'T> =
+    abstract member Get : unit -> 'T
+
+type MyClass() =
+    interface IA<int> with
+        member x.Get() = 1
+    interface IA<string> with
+        member x.Get() = "hello"
+
+let mc = MyClass()
+let iaInt = mc :> IA<int>
+let iaString = mc :> IA<string>
+
+printfn $"{iaInt.Get()}" // 1
+printfn $"{iaString.Get()}" // "hello"
