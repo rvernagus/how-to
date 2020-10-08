db763d2058c98d57d4a5a48c56b600dc8840627b
Ray Vernagus <r.vernagus@gmail.com>
2020-10-07 22:55:20 -0400

    Add comments to C# 9 Records example

--- csharp/9/3-Records/Main.cs
+++ csharp/9/3-Records/Main.cs
@@ -5,7 +5,6 @@ var p1 = new Person { FirstName="Ray", LastName="Vernagus" };
 var p2 = new Person { FirstName="Ray", LastName="Vernagus" };
 Console.WriteLine(p1 == p2);
 
-// Can have constructors
 PersonRecord p3 = new("Ray", "Vernagus");
 PersonRecord p4 = new("Ray", "Vernagus");
 Console.WriteLine(p3 == p4);
@@ -13,7 +12,6 @@ Console.WriteLine(p3 == p4);
 // with expressions
 Console.WriteLine(p3 with { LastName = "Smith" });
 
-// Have a one-line declaration syntax
 // Comes with deconstructor
 var (firstName, lastName) = new PersonRecord2("Ray", "Vernagus");
 Console.WriteLine((firstName, lastName));
@@ -26,11 +24,22 @@ public class Person
 
 public record PersonRecord
 {
+    // Records can have constructors
     public PersonRecord(string firstName, string lastName) =>
         (FirstName, LastName) = (firstName, lastName);
 
+    // Default behavior in with expressions can be overridden
+    protected PersonRecord(PersonRecord original)
+    {
+        Console.WriteLine("  ! This is used in with expressions");
+        FirstName = original.FirstName;
+        LastName = original.LastName;
+    }
+
     public string FirstName { get; init; }
-    public string LastName { get; init; }
+    // Shorthand version of above
+    public string LastName;
 }
 
+// Records have a one-line declaration syntax
 public record PersonRecord2(string FirstName, string LastName);
