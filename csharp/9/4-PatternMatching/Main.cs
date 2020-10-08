f83f5bfd028dd5ce729de7bfdfe1002bcbfc20c3
Ray Vernagus <r.vernagus@gmail.com>
2020-10-07 22:55:48 -0400

    Add C#9 Pattern Matching example

---
+++ csharp/9/4-PatternMatching/Main.cs
@@ -0,0 +1,38 @@
+using System;
+
+static decimal CalculateToll(object vehicle) =>
+    vehicle switch
+    {
+        Car             => 2.00m, // Simple type patterns
+        Taxi            => 3.50m,
+        Bus             => 5.00m,
+        DeliveryTruck t => t.GrossWeightClass switch
+        {
+            > 5000      => 10.00m + 5.00m,
+            < 3000      => 10.00m - 2.00m,
+            _           => 10.00m,
+        },
+        null            => throw new ArgumentNullException(nameof(vehicle)),
+        _               => throw new ArgumentException("Not a known vehicle type", nameof(vehicle))
+    };
+
+public class DeliveryTruck
+{
+    public int GrossWeightClass { get; set; }
+}
+
+public class Car
+{
+    public int Passengers { get; set; }
+}
+
+public class Taxi
+{
+    public int Fares { get; set; }
+}
+
+public class Bus
+{
+    public int Capacity { get; set; }
+    public int Riders { get; set; }
+}
